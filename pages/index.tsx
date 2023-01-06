72cd5db26bb5aaa4d1da1ae89cff37f26fd0b9fe
Selcuk Cihan <selcukcihan@gmail.com>
2023-01-06 16:27:46 +0300

    chore: stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,36 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import { faker } from '@faker-js/faker'
 
-const inter = Inter({ subsets: ['latin'] })
+// const content = (idx: number) => <div key={idx.toString()}><a href="https://twitter.com/gunnarmorling/status/1610203324963516417">@{faker.name.firstName().toLowerCase()}</a>&nbsp;wrote&nbsp;<blockquote className={styles.quoted}>{faker.lorem.paragraph(2).substring(0, 280)}</blockquote></div>
 
-export default function Home() {
+type Tweet = {
+  link: string
+  text: string
+  author: string
+}
+
+type TweetsWithSummary = {
+  summary: string
+  tweets: Tweet[]
+}
+
+export async function getStaticProps(): Promise<{ props: { tweetsWithSummary: TweetsWithSummary }}> {
+  return {
+    props: {
+      tweetsWithSummary: {
+        summary: faker.lorem.paragraph(10),
+        tweets: [...Array(100)].map(_ => ({
+          text: faker.lorem.paragraph(2).substring(0, 280),
+          link: "https://twitter.com/gunnarmorling/status/1610203324963516417",
+          author: faker.name.firstName().toLowerCase(),
+        })),
+      },
+    },
+  }
+}
+
+export default function Home({ tweetsWithSummary }: { tweetsWithSummary: TweetsWithSummary}) {
   return (
     <>
       <Head>
@@ -15,107 +40,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+        <div className={styles.summary}>
+          {tweetsWithSummary.summary}
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div>
+          {tweetsWithSummary.tweets.map((t, idx) => <div key={idx.toString()}><a href={t.link}>@{t.author}</a>&nbsp;wrote&nbsp;<blockquote className={styles.quoted}>{t.text}</blockquote></div>)}
         </div>
       </main>
     </>
