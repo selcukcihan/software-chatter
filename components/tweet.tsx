0ace36089a25eebd4a5802868b7d39870512cfe1
Selcuk Cihan <selcukcihan@gmail.com>
2023-01-11 18:28:08 +0300

    feat: something

---
+++ components/tweet.tsx
@@ -0,0 +1,23 @@
+import { Tweet } from '../types/tweet'
+
+const TweetComponent = ({ tweet }: { tweet: Tweet }) => {
+  return (
+    <section id={`${tweet.index}`}>
+      <div className='bg-gray-50 rounded ring-1 ring-orange-400 border-2 border-orange-300 my-8 mx-auto p-2 max-w-lg shadow-xl shadow-gray-400 opacity-90 mix-blend-darken'>
+        <div>
+          <div className='flex'>
+            <a className='group' href={tweet.link}>
+              <span className='text-sm italic font-light opacity-60 group-hover:opacity-100 group-hover:font-normal'>{tweet.author}</span>
+            </a>
+          </div>
+        </div>
+        <div className='my-2'>{tweet.text}</div>
+        <div className='flex mt-2 gap-8 text-xs font-light justify-end'>
+          <span>{tweet.likes} likes</span>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default TweetComponent
---
+++ types/tweet.ts
@@ -0,0 +1,9 @@
+type Tweet = {
+  link: string
+  text: string
+  author: string
+  likes: number
+  index: number
+}
+
+export type { Tweet }
