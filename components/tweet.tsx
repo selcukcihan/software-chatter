0bdfc2abf5e08531ae5cf91e006f7279f5cab9a0
Selcuk Cihan <selcukcihan@gmail.com>
2023-01-11 18:33:02 +0300

    feat: open in new tab

--- components/tweet.tsx
+++ components/tweet.tsx
@@ -6,7 +6,7 @@ const TweetComponent = ({ tweet }: { tweet: Tweet }) => {
       <div className='bg-gray-50 rounded ring-1 ring-orange-400 border-2 border-orange-300 my-8 mx-auto p-2 max-w-lg shadow-xl shadow-gray-400 opacity-90 mix-blend-darken'>
         <div>
           <div className='flex'>
-            <a className='group' href={tweet.link}>
+            <a className='group' href={tweet.link} target='_blank' rel="noreferrer">
               <span className='text-sm italic font-light opacity-60 group-hover:opacity-100 group-hover:font-normal'>{tweet.author}</span>
             </a>
           </div>
